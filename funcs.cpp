5cd9b1b8440bde5943edc92f7d05703d904ca64e
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-11-05 18:26:03 -0400

    added functions length and fartherFromOrigin

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,35 @@
+/*
+    Author: Halil Akca
+    Course: CSCI-135
+    Instructor: Zamansky
+    Assignment: Lab 9
+    This program will perform all the tasks
+*/
+
 #include <iostream>
+#include <cmath>
+#include "coord3d.h"
 #include "funcs.h"
 
-// add functions here
+double length(Coord3D *p)
+{
+    double fina = p->x * p->x;
+    fina += p->y * p->y;
+    fina += p->z * p->z;
+    return sqrt(fina);
+}
 
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2)
+{
+    double a = length(p1);
+    double b = length(p2);
+
+    if(a > b)
+    {
+        return p1;
+    }
+    else
+    {
+        return p2;
+    }
+}
--- funcs.h
+++ funcs.h
@@ -1,3 +1,5 @@
 #pragma once
+#include "coord3d.h"
 
-// add prototypes here
+double length(Coord3D *p);
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2);
