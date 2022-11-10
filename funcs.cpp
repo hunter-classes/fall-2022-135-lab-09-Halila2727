bee18ec36355d6997196feafe6f8521ee2045b8b
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-11-10 00:16:27 -0500

    added move function

--- funcs.cpp
+++ funcs.cpp
@@ -33,3 +33,10 @@ Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2)
         return p2;
     }
 }
+
+void move(Coord3D *ppos, Coord3D *pvel, double dt)
+{
+    (*ppos).x = (*ppos).x + (*pvel).x * dt; //ppos->x is the same thing
+    (*ppos).y = (*ppos).y + (*pvel).y * dt;
+    (*ppos).z = (*ppos).z + (*pvel).z * dt;
+}
